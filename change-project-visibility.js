4b09da3bf0d175217728c692e4fdc51169802e6e
hilmantsani <nur_h_tsani_x@telkomsel.co.id>
2025-04-07 07:12:54 +0700

    integrate QG check

--- change-project-visibility.js
+++ change-project-visibility.js
@@ -19,19 +19,19 @@ async function getProjectsByVisibility(visibility) {
     }
 
     const data = await response.json();
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
-    // data = 'violation';
+    data = "violation";
+    data = "violation";
+    data = "violation";
+    data = "violation";
+    data = "violation";
+    data = "violation";
+    data = "violation";
+    data = "violation";
+    data = "violation";
+    data = "violation";
+    data = "violation";
+    data = "violation";
+    data = "violation";
     return data.components;
   } catch (error) {
     console.error("Error fetching projects:", error.message);
